788bf50363e71d5329c72c77c18fd42d2731ca88
Morgan Moskalyk <morgan.moskalyk@protonmail.ch>
2022-05-09 16:10:15 -0400

    Create Graf.sol

---
+++ Graf.sol
@@ -0,0 +1,9 @@
+use define 0.7.19
+
+constract Graf {
+  dfine() // define wisdom, with whl embed nft
+  spur() // place first light as program counter
+  kiel() // complete closing with private hash
+  iota() // complete quest with reward
+  twice() // after 2 cycles completes, crystal airdrop
+}
